988a0c8b9a6c6ab2ca3a2df9648bdfffaa22f726
mdshannuu <136819570+mdshannuu@users.noreply.github.com>
2023-07-10 15:23:30 +0530

    the files are commited to master branch

---
+++ average.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+d={"subjects":["coa","dsp","ior","wt","cd"] ,"shannu":[14,12,14,15,15] ,"mehra":[15,14,13,12,11],"rehmat":[12,13,14,15,11],"maha":[11,12,14,15,10],"kalyani":[12,14,15,11,10]}
+df=pd.DataFrame(d)
+df=pd.DataFrame(d)
+print(df)
+ds=np.mean(df["shannu"])
+print(ds)
+ds1=
+
